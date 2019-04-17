4f59e832b59d909cb74a12778de7522b55419462
DougC5 <42100253+DougC5@users.noreply.github.com>
2019-04-17 10:45:15 -0700

    Add files via upload

---
+++ Restaurant/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function clicked() {
+    console.log('button was clicked');
+    
+}
+
+function SendTest(){
+    let text = $('#txtName').val();
+
+    $.ajax({
+        type: "POST",
+        url: "/Employee/Create",
+        data: text,
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            console.log('POST Ended');
+            console.log('Server Says: ', res);
+            
+        }
+    });
+}
